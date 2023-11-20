2ec46d54196badaa31b526fc5d3edc937f0b3d2e
antoniom57 <antoniojimenez77@aragon.unam.mx>
2023-11-20 01:26:19 -0600

    se cambio la tematica de la BD

--- gimnasio.sql
+++ gimnasio.sql
@@ -1,17 +1,17 @@
-drop schema if exists colegio;
-create schema  if not exists colegio default  character set utf8 collate  utf8_spanish2_ci;
-use colegio; 
+drop schema if exists gimnasio;
+create schema  if not exists gimnasio default  character set utf8 collate  utf8_spanish2_ci;
+use gimnasio; 
 
-create table alumno(
-numero int(9) not null auto_increment primary key,
-clave_alu varchar(9) not null,
+create table clientes(
+id_cliente int(9) not null auto_increment primary key,
 ap_paterno varchar(50) not null,
 ap_materno varchar(50) not null,
 nombre varchar(50) not null,
-curp varchar(18) not null,
 direccion text not null,
 telefono varchar(10) not null,
 email varchar(50) not null,
+fecha_nacimiento date not null,
+genero ENUM('Masculino', 'Femenino', 'Otro') not null,
 nombre_usuario varchar(50) not null,
 contraseña varchar(15) not null,
 fecha_registro datetime not null default current_timestamp
