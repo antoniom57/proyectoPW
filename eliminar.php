c679ec6199220f9cfa05c3e1372e31e03dcde920
antoniom57 <antoniojimenez77@aragon.unam.mx>
2023-11-27 20:48:47 -0600

    cambios para eliminar usuarios

---
+++ eliminar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gold's Gym</title>
+    <link rel="shortcut icon" href="media/img/gym.png" type="image/x-icon">
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body class="deleteuser">
+
+    <div class="eliminar">
+        <h1>Adios :(</h1>
+        <form action="deleteuser.php" method="post">
+            <div class="userdelete">
+                <input name="nombre" type="text" required>
+                <label>Usuario</label>
+            </div>
+            
+            <button class="btndelete">Eliminar usuario</button>
+            
+            
+            
+        </form>
+    </div>
+
+</body>
+</html>
